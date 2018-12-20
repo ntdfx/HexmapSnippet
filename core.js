373cd4621ab318e1e31dede3e2d95dcdd44ff960
ntdfx <nt.derzsi@gmail.com>
2018-12-20 17:37:16 +0100

    Up

--- core.js
+++ core.js
@@ -1,8 +1,6 @@
 (function(){
 
-    function generateLinkForBlock(blockNumber) {
-        return blockNumber + '.html';
-    }
+ 
 
     function hexagonBlockOnClickFinish() {
 
@@ -21,13 +19,13 @@
     } 
     var blockNumberingCurr = 0;
 
-    var blocks = {};
+    var blocks = window.hexblocks = {};
     var offset = {left: 27, top: 15};
     for(var row=0; row<grid[0]; row++) {
         for(var col=0; col<grid[1]; col++) {
             var block = {
                 left: (offset.left + col * 159.5) + (row%2 !== 0 ? 80 : 0),
-                top: offset.top + row * 138,
+                top: offset.top + row * 137.7,
                 missing: true
             };
             blocks[generateBlockNumber(row, col)] = block;
@@ -37,52 +35,61 @@
 
     blocks['0207'].missing = false;
 
+    function generateMap() {
+        for(var blockNumber in blocks) {
+            var block = blocks[blockNumber];
 
-    for(var blockNumber in blocks) {
-        var block = blocks[blockNumber];
-        var indicatorText = block.missing ? "✘" : "✔";
-        var indicatorShadow = createSvgElement('text', {
-            'class': 'ks_hexblock_indicator_shadow',
-            'x': block.left + 82,
-            'y': block.top + 102,
-            'text-anchor': 'middle',
-        });
-        var indicator = createSvgElement('text', {
-            'class': 'ks_hexblock_indicator' + (block.missing ? ' missing' : ''),
-            'x': block.left + 80,
-            'y': block.top + 100,
-            'text-anchor': 'middle',
-        });
-        indicatorShadow.appendChild(document.createTextNode(indicatorText));
-        indicator.appendChild(document.createTextNode(indicatorText));
 
-        hexmap.appendChild(indicatorShadow);
-        hexmap.appendChild(indicator);
-
-        var hexagon = createSvgElement('path', {
-            'id': 'b' + blockNumber,
-            'class': 'ks_hexblock',
-            'd': [
-                ['M'+(block.left+77),block.top+180].join(','),
-                ['L'+(block.left),block.top+134].join(','),
-                ['L'+(block.left),block.top+45].join(','),
-                ['L'+(block.left+77),block.top+0].join(','),
-                ['L'+(block.left+155),block.top+45].join(','),
-                ['L'+(block.left+155),block.top+134].join(','),
-                ['L'+(block.left+77),block.top+180].join(','),
-                ,'Z'].join(' ')
-        });
-
-        if (block.missing) {
-            hexmap.appendChild(hexagon);
-        } else {
-            var link = createSvgElement('a', {
-                'href': block.missing ? '' : generateLinkForBlock(blockNumber),
-                'target': '_blank',
+            var hexagon = createSvgElement('path', {
+                'id': 'b' + blockNumber,
+                'class': 'ks_hexblock' + (block.missing ? ' missing' : ''),
+                'd': [
+                    ['M'+(block.left+77),block.top+180].join(','),
+                    ['L'+(block.left),block.top+134].join(','),
+                    ['L'+(block.left),block.top+45].join(','),
+                    ['L'+(block.left+77),block.top+0].join(','),
+                    ['L'+(block.left+155),block.top+45].join(','),
+                    ['L'+(block.left+155),block.top+134].join(','),
+                    ['L'+(block.left+77),block.top+180].join(','),
+                    ,'Z'].join(' ')
             });
-            link.appendChild(hexagon);
-            hexmap.appendChild(link);
+
+
+
+            if (block.missing) {
+                hexmap.appendChild(hexagon);
+            } else {
+                var link = createSvgElement('a', {
+                    'href': block.missing ? '' : generateLinkForBlock(blockNumber),
+                    'target': '_blank',
+                });
+                link.appendChild(hexagon);
+                hexmap.appendChild(link);
+            }
+            var indicatorText = block.missing ? "✘" : "✔";
+            var indicatorShadow = createSvgElement('text', {
+                'class': 'ks_hexblock_indicator_shadow',
+                'x': block.left + 30+2,
+                'y': block.top + 80+2,
+                'text-anchor': 'middle',
+            });
+            var indicator = createSvgElement('text', {
+                'class': 'ks_hexblock_indicator' + (block.missing ? ' missing' : ''),
+                'x': block.left + 30,
+                'y': block.top + 80,
+                'text-anchor': 'middle',
+            });
+            indicatorShadow.appendChild(document.createTextNode(indicatorText));
+            indicator.appendChild(document.createTextNode(indicatorText));
+
+            hexmap.appendChild(indicatorShadow);
+            hexmap.appendChild(indicator);
+
         }
+
     }
+    setTimeout(function(){
+        generateMap();
+    })
 
 })();
